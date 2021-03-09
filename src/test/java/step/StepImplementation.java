43db6081217205192544a0d3df482534ab8f8f7e
YUSUF-PC\Yusuf-Testinium <yusufbuzrul@gmail.com>
2021-03-09 17:33:31 +0300

    commit

--- src/test/java/step/StepImplementation.java
+++ src/test/java/step/StepImplementation.java
@@ -1,20 +1,34 @@
 package step;
 
+import com.thoughtworks.gauge.BeforeScenario;
 import com.thoughtworks.gauge.Step;
+import entity.user.User;
+import io.restassured.response.Response;
 import methods.UserMethods;
+import org.testng.annotations.BeforeMethod;
+import utility.log;
 
 public class StepImplementation {
 
 
-    UserMethods methods;
+    private UserMethods methods;
+    private User user;
 
-    public StepImplementation() //constructor
-    {
+    @BeforeMethod
+    public void BeforeMethod(){
+        user = new User();
         methods = new UserMethods();
     }
-    @Step("<item> bilgisi gir")
-    public void implementation1(String key) {
-        methods.createdUser();
+
+
+    @Step("Get User login")
+    public void implementation1() {
+        user.setUsername(username); //düzelteceğim
+        user.setPassword(password); // bunu da
+        Response response = methods.createdUser(user);
+        log.info("Response statu code : " + response.getStatusCode());
+        log.info("Response login success");
+    }
 
     }
 }
